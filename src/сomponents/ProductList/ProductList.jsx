9b37c246386ba6fc618357bcacadce9807f8962e
Vol4enak <king5vadim@gmail.com>
2024-05-23 18:39:29 +0300

    Update ProductList.jsx

--- src/сomponents/ProductList/ProductList.jsx
+++ src/сomponents/ProductList/ProductList.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 // import css from "./ProductList.module.css"
-
+import { useTelegram } from "../Hooks/useTelegram";
 const ProductList = () => {
+  const { onToggleButton } = useTelegram;
   return (
     <div>
       ProductList
+      <button onClick={onToggleButton}>toggle</button>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
