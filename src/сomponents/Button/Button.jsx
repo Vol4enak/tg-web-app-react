378ea8a87bb988f0ce0773923ff0e68c25f2845e
Vol4enak <king5vadim@gmail.com>
2024-05-23 19:50:47 +0300

    init

--- src/сomponents/Button/Button.jsx
+++ src/сomponents/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import css from "./button.module.css"
 const Button = (props) => {
-  return <button {...props} className={css.button + props.className}/>;
+  return <button {...props} className={css.button}/>;
 };
 
 export default Button;
--- src/сomponents/Form/Form.jsx
+++ src/сomponents/Form/Form.jsx
@@ -1,62 +1,63 @@
-import React, { useState, useEffect } from "react";
-import css from "./Form.module.css";
-import { useTelegram } from "../Hooks/useTelegram";
+// import React, { useState, useEffect } from "react";
+// import css from "./Form.module.css";
+// import { useTelegram } from "../Hooks/useTelegram";
 const Form = () => {
-  const [country, setCountry] = useState("");
-  const [street, setStreet] = useState("");
-  const [subject, setSubject] = useState("physical");
-  const { tg } = useTelegram();
+//   const [country, setCountry] = useState("");
+//   const [street, setStreet] = useState("");
+//   const [subject, setSubject] = useState("physical");
+//   const { tg } = useTelegram();
 
-  useEffect(() => {
-    tg.MainButton.setParams({
-      text: "отправить данные",
-    });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+//   useEffect(() => {
+//     tg.MainButton.setParams({
+//       text: "отправить данные",
+//     });
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
 
-  useEffect(() => {
-    if (!street || !country) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.show();
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [country, street]);
+//   useEffect(() => {
+//     if (!street || !country) {
+//       tg.MainButton.hide();
+//     } else {
+//       tg.MainButton.show();
+//     }
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [country, street]);
 
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-  const onChangeStreet = (e) => {
-    setStreet(e.target.value);
-  };
+//   const onChangeCountry = (e) => {
+//     setCountry(e.target.value);
+//   };
+//   const onChangeStreet = (e) => {
+//     setStreet(e.target.value);
+//   };
 
-  const onChangeSubject = (e) => {
-    setSubject(e.target.value);
-  };
+//   const onChangeSubject = (e) => {
+//     setSubject(e.target.value);
+//   };
 
   return (
-    <div className={css.form}>
-      <h3>Введите данные</h3>
-      <input
-        type="text"
-        placeholder="country"
-        className={css.input}
-        value={country}
-        onChange={onChangeCountry}
-      />
-      <input
-        type="text"
-        placeholder="city"
-        className={css.input}
-        value={street}
-        onChange={onChangeStreet}
-      />
+    <div>ghbdtn</div>
+    // <div className={css.form}>
+    //   <h3>Введите данные</h3>
+    //   <input
+    //     type="text"
+    //     placeholder="country"
+    //     className={css.input}
+    //     value={country}
+    //     onChange={onChangeCountry}
+    //   />
+    //   <input
+    //     type="text"
+    //     placeholder="city"
+    //     className={css.input}
+    //     value={street}
+    //     onChange={onChangeStreet}
+    //   />
 
-      <select value={subject} onChange={onChangeSubject} className={css.select}>
-        <option value={"physical"}>fiz</option>
-        <option value={"legal"}>ur</option>
-      </select>
-    </div>
+    //   <select value={subject} onChange={onChangeSubject} className={css.select}>
+    //     <option value={"physical"}>fiz</option>
+    //     <option value={"legal"}>ur</option>
+    //   </select>
+    // </div>
   );
 };
 
