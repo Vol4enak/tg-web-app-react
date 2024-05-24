5ee6c2cb9f8bf1a8c48603fe8bc37c027f07d739
Vol4enak <king5vadim@gmail.com>
2024-05-24 17:28:56 +0300

    Update useTelegram.js

--- src/сomponents/Hooks/useTelegram.js
+++ src/сomponents/Hooks/useTelegram.js
@@ -16,6 +16,6 @@ export function useTelegram() {
     onToggleButton,
     tg,
     user: tg.initDataUnsafe?.user,
-    queryId: tg.initDataUnsafe?.query_Id,
+    queryId: tg.initDataUnsafe?.query_id,
   };
 }
