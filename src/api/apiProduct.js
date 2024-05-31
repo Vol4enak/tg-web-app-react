94378566d8813a1c63644925acbc82edf0e8e0ee
Vol4enak <king5vadim@gmail.com>
2024-05-31 17:18:16 +0300

    init

--- src/api/apiProduct.js
+++ src/api/apiProduct.js
@@ -5,3 +5,15 @@ const getPopularMovieFromServer = () => {
 };
 
 export default getPopularMovieFromServer;
+
+// const getPopularMovieFromServer = () => {
+//   return fetch("https://shop-21275.x9.co.ua/api/auth", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json;charset=utf-8",
+//     },
+//   }).then((response) => response.json());
+// };
+
+// export default getPopularMovieFromServer;
+// // cw8iYPMFxrxDFWS
