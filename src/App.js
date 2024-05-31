130db43507dc2554ffd6019e1129b9a9f4e16c3b
Vol4enak <king5vadim@gmail.com>
2024-05-31 11:53:47 +0300

    init

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import Header from "./сomponents/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import ProductList from "./сomponents/ProductList/ProductList";
 import Form from "./сomponents/Form/Form";
-
+import getPopularMovieFromServer from "./api/apiProduct";
 function App() {
+
+  useEffect(() => {
+    const featchData = async () => {
+      const fetch = await getPopularMovieFromServer();
+      console.log(fetch);
+    };
+    featchData().catch(console.error);
+  }, []);
+
   const { tg } = useTelegram();
   useEffect(() => {
     tg.ready();
--- src/api/apiProduct.js
+++ src/api/apiProduct.js
@@ -1,6 +1,7 @@
 const getPopularMovieFromServer = () => {
-  return fetch(
-    `https://api-seller.rozetka.com.ua/market-categories/search?parent_id=4630027`
-  ).then((response) => response.json());
+  return fetch("https://jsonplaceholder.typicode.com/todos").then(
+    (response) => response.json()
+  );
 };
+
 export default getPopularMovieFromServer;
