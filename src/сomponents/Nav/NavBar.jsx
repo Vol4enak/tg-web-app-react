2878c946bb53030a33e3f41e02bfb1d7c02d933f
Vol4enak <king5vadim@gmail.com>
2024-06-01 18:11:30 +0300

    fix style

--- src/сomponents/Nav/NavBar.jsx
+++ src/сomponents/Nav/NavBar.jsx
@@ -12,12 +12,26 @@ function NavBar() {
     <div className={css.navBox}>
       <nav className={css.navBar}>
         <div className={css.burgerMenu} onClick={toggleMenu}>
-          <div className={`${css.burgerBar} ${isMenuActive ? css.clicked : css.unclicked}`}></div>
-          <div className={`${css.burgerBar} ${isMenuActive ? css.clicked : css.unclicked}`}></div>
-          <div className={`${css.burgerBar} ${isMenuActive ? css.clicked : css.unclicked}`}></div>
+          <div
+            className={`${css.burgerBar} ${
+              isMenuActive ? css.clicked : css.unclicked
+            }`}
+          ></div>
+          <div
+            className={`${css.burgerBar} ${
+              isMenuActive ? css.clicked : css.unclicked
+            }`}
+          ></div>
+          <div
+            className={`${css.burgerBar} ${
+              isMenuActive ? css.clicked : css.unclicked
+            }`}
+          ></div>
         </div>
       </nav>
-      <div className={`${css.menu} ${isMenuActive ? css.visible : css.hidden}`}></div>
+      <div
+        className={`${css.menu} ${isMenuActive ? css.visible : css.hidden}`}
+      ></div>
     </div>
   );
 }
